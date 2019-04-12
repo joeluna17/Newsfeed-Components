89db5eb08767f5b83f5770ba2d3e4c44e6f01f1a
Joseph Luna <josephluna@Josephs-MBP.local>
2019-04-12 10:18:43 -0500

    committng MVP + Menu Slide animate using js.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -14,6 +14,35 @@ const menuButton = document.querySelector('.menu-button')
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener('click', toggleMenu)
+//Begin Menu Slide Stretch
+menu.style.display = "block";
+menu.style.left = "-350px"
+
+const slide = () => {
+  let pos = -350;
+  let openPos = 0;
+  let id = setInterval(frame,0.5);
+ 
+  function frame(){
+    if (pos === 0){     
+     clearInterval(id)    
+    }else if (menu.style.left === openPos + 'px'){
+      openPos--;
+      menu.style.left = openPos + "px";
+       if(openPos === pos){ clearInterval(id)}
+    }
+      else{
+        pos++;
+          menu.style.left = pos + "px";
+      }    
+    }
+  }
+
+menuButton.addEventListener('click', slide)
+//End Menu Slide
+//menuButton.addEventListener('click', toggleMenu)  // from MPV code
 
 //
+
+
+
